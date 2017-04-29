811d7f884517e30a71295f6b180ca3c8e4c31858
Steve Bomze <sbomze@chicagobooth.edu>
2017-04-29 12:12:36 -0500

    v1

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,7 @@
 class MoviesController < ApplicationController
   def index
     @movies = Movie.all
+    render("movies/index.html.erb")
   end
 
   def show
@@ -8,19 +9,21 @@ class MoviesController < ApplicationController
   end
 
   def new_form
+    render("movies/new_form.html.erb")
   end
 
   def create_row
-    @movie.title = params[:the_title]
-    @movie.year = params[:the_year]
-    @movie.duration = params[:the_duration]
-    @movie.description = params[:the_description]
-    @movie.image_url = params[:the_image_url]
-    @movie.director_id = params[:the_director_id]
+    @movie = Movie.new
+    @movie.title = params[:title]
+    @movie.year = params[:year]
+    @movie.duration = params[:duration]
+    @movie.description = params[:description]
+    @movie.image_url = params[:image_url]
+    @movie.director_id = params[:director_id]
 
     @movie.save
-
-    render("show")
+    redirect_to("/")
+    #render("show")
   end
 
   def edit_form

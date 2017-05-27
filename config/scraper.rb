2f26c5db4adb6f30e5915dc3e9d8f00d080ca5b4
Steve Bomze <sbomze@chicagobooth.edu>
2017-05-27 12:10:07 -0500

    fp_11

--- config/scraper.rb
+++ config/scraper.rb
@@ -1,21 +1,26 @@
 
 require "mechanize"
+agent = Mechanize.new
+page = agent.get("https://action.aclu.org/secure/donate-to-aclu")
+forms = page.forms
+form = forms.first
 
-url = ARGV[0]
-fp = File.new("wikilinks.txt", "w")
-window.open("https://action.aclu.org/secure/donate-to-aclu")
-agent = Mechanize.new { |agent| agent.user_agent_alias = "Mac Safari" }
+form.radiobutton_with(:name => "submitted[donation][amount]", :value => "other").check
+form['submitted[donation][other_amount]'] = 5
 
-html = agent.get(url).body
+form['submitted[donor_information][first_name]'] = "Steve"
+form['submitted[donor_information][last_name]'] = "Bomze"
+form['submitted[donor_information][email]'] = "steve.bomze@gmail.com"
 
-html_doc = Nokogiri::HTML(html)
+form['submitted[billing_information][zip]'] = 92663
+form['submitted[billing_information][address]'] = "209 40th Street"
+form['submitted[billing_information][address_line_2]'] = "Apt B"
+form['submitted[billing_information][city]'] = "Newport Beach"
+form['submitted[billing_information][state]'] = 12
 
-fp.write("References\n\n")
+form['submitted[credit_card_information][card_number]'] = 4737028980185345
+form['submitted[credit_card_information][expiration_date][card_expiration_month]'] = 8
+form['submitted[credit_card_information][expiration_date][card_expiration_year]'] = 2019
+form['submitted[credit_card_information][card_cvv]'] = 977
 
-list = html_doc.xpath("//ol[@class='references']")
-list.each { |i| fp.write(i.text + "\n") }
-
-fp.write("Further Reading\n\n")
-
-list = html_doc.xpath("//span[@class='citation']")
-list.each { |i| fp.write(i.text + "\n") }
+form.submit
